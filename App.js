f190eefdcb0ae9ad80d77f925296dd08818e0f4b
Ace <58069279+Mix200208@users.noreply.github.com>
2021-06-11 13:34:02 +0300

    AddButton

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {TouchableOpacity,KeyboardAvoidingView, Platform, StyleSheet, Text, View ,TextInput} from 'react-native';
 import { Color } from './src/color';
 import Task from './src/components/Task'
 export default function App() {
@@ -19,10 +19,21 @@ export default function App() {
 
            
 
-          </View>
+            </View>
+            
+        </View>
 
-      </View>
+          <KeyboardAvoidingView behavior = {Platform.OS === "android" ? "height":"padding"} 
+            style = {styles.writeTask}>
+              <TextInput style = {styles.input} placeholder = {"Напишите вашу задачу"}/>
+              <TouchableOpacity>
+                  <View style = {styles.addWrapper}>
+                      <Text style = {styles.addText}>+</Text>
+                  </View>
+              </TouchableOpacity>
+          </KeyboardAvoidingView>
 
+    
     </View>
   );
 }
@@ -48,16 +59,48 @@ const styles = StyleSheet.create({
     color:"black",
     paddingLeft:100,
     paddingBottom:10
-    
-    
-    
-
-    
+        
   },
   elements:{
     padding:0,
 
-  }
+  },
+  writeTask:{
+    position:"absolute",
+    bottom:60,
+    width: "100%",
+    flexDirection:"row",
+    justifyContent:"space-around",
+    alignItems: "center"
+    
+  },
+  
+  input:{
+
+    paddingVertical : 15,
+    width: 250,
+    paddingHorizontal:20,
+    backgroundColor:"white",
+    borderRadius:60,
+    borderColor: Color.TASK_ITEM,
+    borderWidth: 0.5
+    
+    
+
+  },
+  addWrapper:{
+    
+    width: 60,
+    height:60,
+    backgroundColor:"white",
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:60,
+    borderColor: Color.TASK_ITEM,
+    borderWidth: 0.5
+
+  },
+  addText:{}
 
 
   
